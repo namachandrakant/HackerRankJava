af24f85eb6accd109fcc74a01a3846bc3340e0d1
namachandrakant <34737661+namachandrakant@users.noreply.github.com>
2017-12-23 12:53:10 +0530

    Create JavaStringsIntroduction.java
    https://www.hackerrank.com/challenges/java-strings-introduction/problem

---
+++ JavaStringsIntroduction.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+    }
+}
